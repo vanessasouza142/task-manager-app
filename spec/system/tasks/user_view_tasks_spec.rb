60fadf37dc21c3afeae6b1ae582730af0e1aab27
vanessasouza142 <vanessa.kbs16@gmail.com>
2024-10-18 00:11:46 -0300

    User view tasks list

--- spec/system/tasks/user_view_tasks_spec.rb
+++ spec/system/tasks/user_view_tasks_spec.rb
@@ -1,19 +1,18 @@
 require 'rails_helper'
 
-describe 'User view task details' do
+describe 'User view task list' do
   it 'and must be authenticated' do
     #Arrange
-    task = Task.create!(url: 'https://www.webmotors.com.br/comprar/chevrolet')
-
+    
     #Act
-    visit task_path(task)
+    visit tasks_path
     
     #Assert
     expect(current_path).to eq root_path
     expect(page).to have_content 'Você não tem permissão para realizar essa ação!'
   end
 
-  it 'from home' do
+  it 'from home successfully' do
     #Arrange
     stub_request(:post, "#{ENV['AUTH_SERVICE_URL']}/api/v1/register")
     .with(
@@ -25,19 +24,21 @@ describe 'User view task details' do
       body: { user: { name: 'Maria da Silva', email: 'mariadasilva@mailinator.com' }, token: 'fake_token' }.to_json,
       headers: { 'Content-Type' => 'application/json' }
     )
-    task = Task.create!(url: 'https://www.webmotors.com.br/comprar/chevrolet')
+    task1 = Task.create!(url: 'https://www.webmotors.com.br/comprar/chevrolet')
+    task2 = Task.create!(url: 'https://www.webmotors.com.br/comprar/toyota')
+    task2 = Task.create!(url: 'https://www.webmotors.com.br/comprar/fiat')
     
     #Act
     visit root_path
     fill_in 'E-mail', with: 'mariadasilva@mailinator.com'
     fill_in 'Senha', with: 'password'
     click_on 'Entrar'
-    click_on 'Visualizar'
     
     #Assert
-    expect(current_path).to eq task_path(task)
-    expect(page).to have_content "Tarefa ##{task.id}"
-    expect(page).to have_content 'Resultado da Tarefa'
+    expect(current_path).to eq tasks_path
+    expect(page).to have_content 'Lista de Tarefas'
+    expect(page).to have_content 'https://www.webmotors.com.br/comprar/chevrolet'
+    expect(page).to have_content 'https://www.webmotors.com.br/comprar/toyota'
+    expect(page).to have_content 'https://www.webmotors.com.br/comprar/fiat'
   end
-
 end
